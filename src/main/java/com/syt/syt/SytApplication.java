e0497366cd85b44c74f5c834cfbb9ecb491f4146
foofoo3 <3139640799@qq.com>
2023-02-05 00:53:55 +0800

    Initial commit

---
+++ src/main/java/com/syt/syt/SytApplication.java
@@ -0,0 +1,13 @@
+package com.syt.syt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SytApplication.class, args);
+    }
+
+}
