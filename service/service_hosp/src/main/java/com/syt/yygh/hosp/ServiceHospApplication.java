2942f49e159b89230eb5ea95d5ae46455021a788
foofoo3 <3139640799@qq.com>
2023-02-08 22:07:15 +0800

    1

---
+++ service/service_hosp/src/main/java/com/syt/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.syt.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: foofoo3
+ */
+
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
