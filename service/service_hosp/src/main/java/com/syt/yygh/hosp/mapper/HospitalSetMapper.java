bc4445ef791a288416e020537aebefcaf16558e4
foofoo3 <3139640799@qq.com>
2023-02-09 21:41:00 +0800

    2,9

---
+++ service/service_hosp/src/main/java/com/syt/yygh/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,10 @@
+package com.syt.yygh.hosp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import syt.hospital.model.hosp.HospitalSet;
+
+/**
+ * @Author: foofoo3
+ */
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+}
