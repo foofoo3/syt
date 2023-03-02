03cf2561b9b57b58909a12912d3bc3d64b46dfa4
foofoo3 <3139640799@qq.com>
2023-03-02 20:33:28 +0800

    32

--- service/service_hosp/src/main/java/com/syt/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/syt/yygh/hosp/ServiceHospApplication.java
@@ -2,12 +2,14 @@ package com.syt.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author: foofoo3
  */
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.syt")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- service/service_hosp/src/main/java/com/syt/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/syt/yygh/hosp/controller/HospitalSetController.java
@@ -1,6 +1,8 @@
 package com.syt.yygh.hosp.controller;
 
 import com.syt.yygh.hosp.service.HospitalSetService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import syt.hospital.model.hosp.HospitalSet;
@@ -11,6 +13,7 @@ import java.util.List;
  * @Author: foofoo3
  */
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospital")
 public class HospitalSetController {
@@ -18,12 +21,14 @@ public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
 
+    @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet() {
         List<HospitalSet> list = hospitalSetService.list();
         return list;
     }
 
+    @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("{id}")
     public boolean removeHospSet(@PathVariable Long id){
         boolean removeById = hospitalSetService.removeById(id);
