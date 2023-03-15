52460c9df7757e6b35c49da121b38c5ec24fed5f
foofoo3 <3139640799@qq.com>
2023-03-16 02:17:44 +0800

    Update UserInfoServiceImpl.java

--- service/service_user/src/main/java/com/syt/yygh/user/service/impl/UserInfoServiceImpl.java
+++ service/service_user/src/main/java/com/syt/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -3,10 +3,12 @@ package com.syt.yygh.user.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.syt.yygh.user.mapper.UserInfoMapper;
 import com.syt.yygh.user.service.UserInfoService;
+import org.springframework.stereotype.Service;
 import syt.hospital.model.user.UserInfo;
 
 /**
  * @Author: foofoo3
  */
+@Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService {
 }
